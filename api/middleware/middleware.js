fcb8569ecc1709d409e024040a43a72edab78b82
cainpalmer <cain.a.palmer@gmail.com>
2021-12-01 17:06:02 -0500

    finished

--- api/middleware/middleware.js
+++ api/middleware/middleware.js
@@ -1,17 +1,45 @@
+
+// Middleware Variables
+const User = require('../users/users-model');
+const Post = require('../posts/posts-model');
+
+// Middlewares
 function logger(req, res, next) {
-  // DO YOUR MAGIC
+  console.log(req.method, req.url, Date.now())
+  next()
 }
 
 function validateUserId(req, res, next) {
-  // DO YOUR MAGIC
+  const {id} = req.params;
+  User.getById(id)
+  .then(user => {
+    if (user) {
+      req.user = user;
+      next();
+    } else {
+      res.status(404).json({message: 'user not found'});
+    }
+  })
+  .catch(err => {
+    res.status(404).json('error retrieving from database');
+  });
 }
 
 function validateUser(req, res, next) {
-  // DO YOUR MAGIC
+  if (!req.body.name) {
+    res.status(400).json({message: 'missing required name field'});
+  } else {
+    next();
+  }
 }
 
 function validatePost(req, res, next) {
-  // DO YOUR MAGIC
+  if (!req.body.text) {
+    res.status(400).json({message: 'missing required text field'});
+  } else {
+    next();
+  }
 }
 
-// do not forget to expose these functions to other modules
+// Exports
+module.exports = {logger, validateUserId, validateUser, validatePost};
